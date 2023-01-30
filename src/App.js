0a3fe4bb20788d5e5efbadb7c760aafb31fba773
vandana <vandana123annusah@gmail.com>
2023-01-30 13:19:58 +0530

    single-page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Header from "./Components/Header";
-import Main from "./Components/Main"
+import Main from "./Components/Main";
 import Hero from "./Components/Hero";
 import Experience from "./Components/Experience";
 import Mission from "./Components/Mission";
-import Team from './Components/Team'
+import Team from './Components/Team';
 import TeamsMem from "./Components/TeamsMem";
 import Contact from "./Components/Contact";
-import Footer from "./Components/Footer.jsx"
+import { Route, Routes } from "react-router-dom";
+import Signin from './Components/Signin';
+import Footer from "./Components/Footer.jsx";
 
 function App() {
   return (
+
     <div className="App">
+      <Routes>
+      <Route path="/signin" element={<Signin />} />
+      </Routes>
       <Header />
       <Hero />
       <Main />
